0bdad40d0e98348e72fba31c311f78accaed29ff
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-08-05 15:32:23 -0400

    Fix bugs and remove pseudo
    Bugs squashed:
    	- Form submission wasn't validating.
    	- checkDuplicates wasn't telling users why a book wouldn't submit.
    	- removeBook always took from the top of the list, no matter what.
    	- read-status-btn wasn't actually a button.

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ const ctYank = new Book("A Connecticut Yankee in King Arthur's Court", "Mark Twa
 
 
 // -- On-Screen Stuff -- //
-
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pageCountInput = document.getElementById("page-count");
+const readStatusInput = document.getElementById("read-status");
+const modal = document.querySelector("#modal");
+const form = document.querySelector("form");
+const duplicateError = document.querySelector(".duplicate-error");
 
 
 // -------------------------------------------------------------------------------- //
@@ -62,7 +68,7 @@ function createCard(book) {
 	<div class="title">${book.title}</div>
 	<div class="author">${book.author}</div>
 	<div class="page-count">${book.pageCount}</div>
-	<div class="read-status">${book.readStatus ? "Read" : "Not Read"}</div>
+	<button class="read-status-btn">${book.readStatus ? "Read" : "Not Read"}</button>
 	<button type="button" class="remove-btn">X</button>
 	` // End innerHTML
 	return card
@@ -70,30 +76,38 @@ function createCard(book) {
 
 function submitNewBook(e) {
 	e.preventDefault();
-
-	const title = e.target.form[1].value;
-	const author = e.target.form[2].value;
-	const pageCount = e.target.form[3].value;
-	const readStatus= e.target.form[4].checked;
-
-	const newBook = new Book(title, author, pageCount, readStatus);
-
-	if (checkInLibrary(newBook)) {
-		throw new Error("You already have that book!");
+	
+	const newBook = new Book(titleInput.value, authorInput.value, pageCountInput.value, readStatusInput.checked);
+	
+	if (checkDuplicate(newBook)) {
+		duplicateError.classList = "duplicate-error";
+		titleInput.addEventListener("keydown", removeDuplicateError);
+		authorInput.addEventListener("keydown", removeDuplicateError);
 	} else {
-		addBookToLibrary(newBook);			//		//////////////////////////	
-		modal.classlist.toggle("hidden"); // <----- Not recognizing modal here
-		updateGrid();					//			\\\\\\\\\\\\\\\\\\\\\\\\\\
+		addBookToLibrary(newBook);
+		modal.classList.toggle("hidden");
+		updateGrid();
 	}
 }
 
-function checkInLibrary(newBook) {
-	return myLibrary.find(book => book.title.toLowerCase() === newBook.title.toLowerCase() && book.author.toLowerCase() === newBook.author.toLowerCase());
+function checkDuplicate(newBook) {
+	return myLibrary.find(book => {
+		let details = [book.title, newBook.title, book.author, newBook.author].map(detail => detail.toLowerCase().replace(/[^\w\d]/gi, ""));
+		return new Set(details).size < 3;
+	});
+}
+
+function removeDuplicateError(e) {
+	if (e.key.length < 2 || e.key === "Backspace") {
+		duplicateError.classList = "duplicate-error hidden";
+		titleInput.removeEventListener("keydown", removeDuplicateError);
+		authorInput.removeEventListener("keydown", removeDuplicateError);
+	}
 }
 
 function removeBook(e) {
 	const targetBookIndex = getBookIndex(e);
-	myLibrary.splice(myLibrary[targetBookIndex], 1);
+	myLibrary.splice(targetBookIndex, 1);
 	updateGrid();
 }
 
@@ -117,12 +131,9 @@ function checkModalTrigger(target) {
 }
 
 function handleClick(e) {
-	const modal = document.querySelector("#modal");
 	const target = e.target;
 	if (checkModalTrigger(target)) {
 		modal.classList.toggle("hidden");
-	} else if (target.classList.contains("submit-btn")) {
-		submitNewBook(e);
 	} else if (target.classList.contains("remove-btn")) {
 		removeBook(e);
 	} else if (target.classList.contains("read-status-btn")) {
@@ -143,118 +154,5 @@ addBookToLibrary(howlCastle, hobbit, colorOfMagic, hogfather, ctYank);
 // -- On-Screen Stuff -- //
 updateGrid();
 
+form.addEventListener("submit", submitNewBook)
 document.addEventListener("click", handleClick)
-
-
-
-// -------------------------------------------------------------------------------- //
-// ----------------------------     Pseudo     ------------------------------------ //
-// -------------------------------------------------------------------------------- //
-
-/*
-
-Create an array of book objects.
-Create a card to display book info.
-Display a grid of cards with book info.
-Take input from a form, create a new book from those inputs, and refresh the grid with the new book.
-Remove a book from the library, refresh the grid without the deleted book.
-Toggle the read status of any given book.
-
-FUNCTION updateGrid()
-	SELECT currentGrid from DOM (every time to make sure it's the right one)
-	const newGrid = createNewGrid()
-	currentGrid.replaceChild(...newGrid)
-END FUNCTION
-
-FUNCTION createNewGrid()
-	newGrid = []
-	FOR EACH book IN myLibrary
-		const newCard = createCard(book)
-		PUSH newCard to newGrid
-	END FOR
-	RETURN newGrid
-END FUNCTION
-
-FUNCTION createCard(book)
-	card = CREATE <div.card>
-	card.innerHTML = template string with card contents
-
-	Or:
-	FOR EACH key IN book		
-		IF key === readStatus
-			CREATE readStatusBtn with text "Read" or "Not Read"
-			APPEND readStatusBtn to <div.card> 
-		ELSE
-			CREATE <p.key>
-			APPEND <p.key> to <div.card>
-		END IF/ELSE
-	END FOR
-	CREATE removeBtn
-	APPEND removeBtn to <div.card>
-END FUNCTION
-
-FUNCTION submitNewBook(e)
-	e.preventDefault()
-
-	const title = e.target.form[1].value;
-	const author = e.target.form[2].value;
-	const pageCount = e.target.form[3].value;
-	const readStatus= e.target.form[4].checked;
-
-	const newBook = new Book(title, author, pageCount, readStatus)
-
-	IF checkInLibrary(newBook)
-		DISPLAY error on form above inputs
-	ELSE 
-		addBookToLibrary(newBook)
-		HIDE modal
-		updateGrid()
-	END IF/ELSE
-END FUNCTION
-
-FUNCTION checkInLibrary(newBook)
-	return myLibrary.find(book => book.title === newBook.title && book.author === newBook.author);
-END FUNCTION
-
-FUNCTION removeBook(e)
-	targetBookIndex = getBookIndex(e)
-	myLibrary.splice(myLibrary[targetBookIndex], 1)
-	updateGrid()
-END FUNCTION
-
-FUNCTION getBookIndex(e)
-	const targetTitle = e.target.parentElement.children[0].textContent;
-	const targetAuthor = e.target.parentElement.children[1].textContent;
-	const targetBook = myLibrary.find(book => book.title === targetTitle && book.author === targetAuthor);
-	return myLibrary.indexOf(targetBook)
-END FUNCTION
-
-FUNCTION triggerToggleReadStatus(e)
-	targetBookIndex = getBookIndex(e);
-	myLibrary[targetBookIndex].toggleReadStatus();
-	updateGrid();
-END FUNCTION
-
-FUNCTION handleClick(e)
-	const target = e.target;
-	IF checkModalTrigger(target)
-		modal.classlist.toggle("hidden")
-	ELSE IF target.classList.contains("submit-btn")
-		submitNewBook(e)
-	ELSE IF target.classList.contains("remove-btn")
-		removeBook(e)
-	ELSE IF target.classList.contains("read-status-btn")
-		toggleReadStatus(e)
-	END IF/ELSE
-END FUNCTION
-
-FUNCTION checkModalTrigger(target)
-	const modal = document.querySelector("#modal")
-	RETURN (target === modal) || (target.classList.contains("add-book-btn")) || (target.classList.contains("close-form-btn")) 
-END FUNCTION
-
-EVENT LISTENER document ON CLICK:
-	handleClick(e)
-END EVENT LISTENER
-
-*/

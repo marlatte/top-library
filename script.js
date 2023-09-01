1b68c80f62d363eeb360594d47e0578b12471fa2
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-09-01 17:07:38 -0400

    Add ScreenController module

--- script.js
+++ script.js
@@ -1,178 +1,161 @@
 // -------------------------------------------------------------------------------- //
-// ---------------------------     Variables    ----------------------------------- //
+// ----------------------------     Library Module     ---------------------------- //
 // -------------------------------------------------------------------------------- //
 
+function Library() {
+	let myBooks = [];
+	const getBooks = () => myBooks;
+	
+	class Book {
+		constructor(title, author, pageCount, readStatus) {
+			this.title = title;
+			this.author = author;
+			this.pageCount = pageCount;
+			this.readStatus = readStatus;
+		}
 
-// -- Behind the Scenes -- //
-let myLibrary = [];
-
-
-// -- On-Screen Stuff -- //
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const pageCountInput = document.getElementById("page-count");
-const readStatusInput = document.getElementById("read-status");
-const modal = document.querySelector("#modal");
-const form = document.querySelector("form");
-const duplicateError = document.querySelector(".duplicate-error");
-
-
-// -------------------------------------------------------------------------------- //
-// ----------------------     Functions & Methods    ------------------------------ //
-// -------------------------------------------------------------------------------- //
-
-
-// -- Behind the Scenes -- //
-class Book {
-	constructor(title, author, pageCount, readStatus) {
-		this.title = title;
-		this.author = author;
-		this.pageCount = pageCount;
-		this.readStatus = readStatus;
+		toggleReadStatus() {
+			this.readStatus = !this.readStatus
+		};
 	}
 
-	toggleReadStatus() {
-		this.readStatus = !this.readStatus
-	};
-}
 
-
-function addBookToLibrary(...books) {
-	myLibrary.push(...books);
-}
-
-
-// -- On-Screen Stuff -- //
-function updateGrid() {
-	const currentGrid = document.querySelector(".grid-container");
-	const newGrid = createNewGrid();
-	currentGrid.replaceChildren(...newGrid);
-}
-
-function createNewGrid() {
-	newGrid = []
-	for (const book of myLibrary) {
-		const newCard = createCard(book);
-		newGrid.push(newCard);
+	function addBookToLibrary(cleanDetails) {
+		const newBook = new Book(cleanDetails[0], cleanDetails[1], cleanDetails[2], cleanDetails[3]);
+		myBooks.push(newBook);
 	}
-	return newGrid
+
+	function checkDuplicate(newTitle, newAuthor) {
+		return myBooks.find(book => {
+			let details = [book.title, newTitle, book.author, newAuthor].map(detail => detail.toLowerCase().replace(/[^\w\d]/gi, ""));
+			return new Set(details).size < 3;
+		});
+	}
+
+	function getBookIndex(target) {
+		const targetAuthor = target.parentElement.parentElement.firstElementChild.firstElementChild.children[0].textContent;
+		const targetTitle = target.parentElement.parentElement.firstElementChild.firstElementChild.children[1].textContent;
+		const targetBook = myBooks.find(book => book.title === targetTitle && book.author === targetAuthor);
+		return myBooks.indexOf(targetBook);
+	}
+
+	const startingBooks = [
+		["Howl's Moving Castle", "Diana Wynne Jones", 429, true],
+		["The Hobbit", "J. R. R. Tolkien", 295, true],
+		["The Color of Magic", "Terry Pratchett", 288, true],
+		["Hogfather", "Terry Pratchett", 384, false],
+		["A Connecticut Yankee in King Arthur's Court", "Mark Twain", 469, true]
+	];
+
+	startingBooks.forEach(book => addBookToLibrary(book));
+
+	return { getBooks, getBookIndex, checkDuplicate, addBookToLibrary }
 }
 
-function createCard(book) {
-	const card = document.createElement("div");
-	card.classList = `card read-${book.readStatus}`;
-	card.innerHTML = `
-	<div class="card-top">
-		<div class="highlights">
-			<div class="author">${book.author}</div>
-			<div class="title">${book.title}</div>
+const screenController = (() => {
+	const modal = document.querySelector("#modal");
+	const form = document.querySelector("form");
+	const duplicateError = document.querySelector(".duplicate-error");
+
+	const titleInput = document.getElementById("title");
+	const authorInput = document.getElementById("author");
+	const pageCountInput = document.getElementById("page-count");
+	const readStatusInput = document.getElementById("read-status");
+
+	const library = Library();
+	const myBooks = library.getBooks();
+
+	function updateGrid() {
+		const currentGrid = document.querySelector(".grid-container");
+		const newGrid = createNewGrid();
+		currentGrid.replaceChildren(...newGrid);
+	}
+
+	function createNewGrid() {
+		newGrid = []
+		for (const book of myBooks) {
+			const newCard = createCard(book);
+			newGrid.push(newCard);
+		}
+		return newGrid
+	}
+
+	function createCard(book) {
+		const card = document.createElement("div");
+		card.classList = `card read-${book.readStatus}`;
+		card.innerHTML = `
+		<div class="card-top">
+			<div class="highlights">
+				<div class="author">${book.author}</div>
+				<div class="title">${book.title}</div>
+			</div>
+			<button type="button"
+					class="remove-btn">×
+			</button>
 		</div>
-		<button type="button"
-				class="remove-btn">×
-		</button>
-	</div>
-	<div class="card-bottom">
-		<div class="page-count">${book.pageCount} Pages</div>
-		<div class="read-status-text">Read?</div>
-		<button class="read-status-btn">${book.readStatus ? "Yes" : "No"}</button>
-	</div>
-	` // End innerHTML
-	return card
-}
+		<div class="card-bottom">
+			<div class="page-count">${book.pageCount} Pages</div>
+			<div class="read-status-text">Read?</div>
+			<button class="read-status-btn">${book.readStatus ? "Yes" : "No"}</button>
+		</div>
+		` // End innerHTML
+		return card
+	}
 
-function submitNewBook(e) {
-	e.preventDefault();
+	function handleFormSubmit(e) {
+		e.preventDefault();
 
-	const sanitizedDetails = [titleInput.value, authorInput.value].map(input => {
-		input.replace(/[;:<>{}[\]\\\/]/g, "")
-	});
-	const newBook = new Book(
-		sanitizedDetails[0],
-		sanitizedDetails[1],
-		pageCountInput.value,
-		readStatusInput.checked
-	);
+		const cleanDetails = [titleInput.value, authorInput.value].map(input => input.replace(/[;:<>{}[\]\\\/]/g, ""));
+		cleanDetails.push(pageCountInput.value, readStatusInput.checked);
 
-	if (checkDuplicate(newBook)) {
-		duplicateError.classList = "duplicate-error";
-		titleInput.addEventListener("keydown", removeDuplicateError);
-		authorInput.addEventListener("keydown", removeDuplicateError);
-	} else {
-		addBookToLibrary(newBook);
-		modal.classList.toggle("hidden");
-		form.reset();
+		if (Library().checkDuplicate(cleanDetails[0], cleanDetails[1])) {
+			duplicateError.classList = "duplicate-error";
+			titleInput.addEventListener("keydown", hideDuplicateError);
+			authorInput.addEventListener("keydown", hideDuplicateError);
+		} else {
+			library.addBookToLibrary(cleanDetails);
+			modal.classList.toggle("hidden");
+			form.reset();
+			updateGrid();
+		}
+	}
+
+	function hideDuplicateError(e) {
+		if (e.key.length < 2 || e.key === "Backspace") {
+			duplicateError.classList = "duplicate-error hidden";
+			titleInput.removeEventListener("keydown", hideDuplicateError);
+			authorInput.removeEventListener("keydown", hideDuplicateError);
+		}
+	}
+
+	function checkModalTrigger(target) {
+		return (target === modal) ||
+			(target.classList.contains("add-book-btn")) ||
+			(target.classList.contains("close-form-btn"));
+	}
+
+	function removeBook(target) {
+		const targetBookIndex = Library().getBookIndex(target);
+		myBooks.splice(targetBookIndex, 1);
 		updateGrid();
 	}
-}
 
-function checkDuplicate(newBook) {
-	return myLibrary.find(book => {
-		let details = [book.title, newBook.title, book.author, newBook.author].map(detail => detail.toLowerCase().replace(/[^\w\d]/gi, ""));
-		return new Set(details).size < 3;
-	});
-}
-
-function removeDuplicateError(e) {
-	if (e.key.length < 2 || e.key === "Backspace") {
-		duplicateError.classList = "duplicate-error hidden";
-		titleInput.removeEventListener("keydown", removeDuplicateError);
-		authorInput.removeEventListener("keydown", removeDuplicateError);
+	function handleClick(e) {
+		const target = e.target;
+		const targetBookIndex = getBookIndex(target);
+		if (checkModalTrigger(target)) {
+			modal.classList.toggle("hidden");
+		} else if (target.classList.contains("remove-btn")) {
+			removeBook(target);
+		} else if (target.classList.contains("read-status-btn")) {
+			myBooks[targetBookIndex].toggleReadStatus();
+			updateGrid();
+		}
 	}
-}
 
-function removeBook(e) {
-	const targetBookIndex = getBookIndex(e);
-	myLibrary.splice(targetBookIndex, 1);
 	updateGrid();
-}
 
-function getBookIndex(target) {
-	const targetAuthor = target.parentElement.parentElement.firstElementChild.firstElementChild.children[0].textContent;
-	const targetTitle = target.parentElement.parentElement.firstElementChild.firstElementChild.children[1].textContent;
-	const targetBook = myLibrary.find(book => book.title === targetTitle && book.author === targetAuthor);
-	return myLibrary.indexOf(targetBook);
-}
+	form.addEventListener("submit", handleFormSubmit)
+	document.addEventListener("click", handleClick)
 
-function triggerToggleReadStatus(target) {
-	const targetBookIndex = getBookIndex(target);
-	myLibrary[targetBookIndex].toggleReadStatus();
-	updateGrid();
-}
-
-function checkModalTrigger(target) {
-	return (target === modal) ||
-		(target.classList.contains("add-book-btn")) ||
-		(target.classList.contains("close-form-btn"));
-}
-
-function handleClick(e) {
-	const target = e.target;
-	if (checkModalTrigger(target)) {
-		modal.classList.toggle("hidden");
-	} else if (target.classList.contains("remove-btn")) {
-		removeBook(target);
-	} else if (target.classList.contains("read-status-btn")) {
-		triggerToggleReadStatus(target);
-	}
-}
-
-
-// -------------------------------------------------------------------------------- //
-// --------------------     Calls & Event Listeners    ---------------------------- //
-// -------------------------------------------------------------------------------- //
-
-
-// -- Behind the Scenes -- //
-const howlCastle = new Book("Howl's Moving Castle", "Diana Wynne Jones", 429, true);
-const hobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295, true);
-const colorOfMagic = new Book("The Color of Magic", "Terry Pratchett", 288, true);
-const hogfather = new Book("Hogfather", "Terry Pratchett", 384, false);
-const ctYank = new Book("A Connecticut Yankee in King Arthur's Court", "Mark Twain", 469, true);
-addBookToLibrary(howlCastle, hobbit, colorOfMagic, hogfather, ctYank);
-
-
-// -- On-Screen Stuff -- //
-updateGrid();
-
-form.addEventListener("submit", submitNewBook)
-document.addEventListener("click", handleClick)
+})();
